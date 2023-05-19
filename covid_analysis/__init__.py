96d27c6d28dbcaf5db49e0c57ee231e285af0d3f
plee <pleelaprachakul@headstorm.com>
2023-05-19 11:46:09 -0400

    fix: internal module file path

--- covid_analysis/__init__.py
+++ covid_analysis/__init__.py
@@ -1,1 +1,6 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 __version__ = "0.0.1"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,4 @@
 import sys
-sys.path.append('.')
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
