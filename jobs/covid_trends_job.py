1276dafb9e1a269d4fe53d3ba5ac405b9566ad33
plee <pleelaprachakul@headstorm.com>
2023-05-19 12:37:41 -0400

    add ml and adjust project dbx setting

--- jobs/covid_trends_job.py
+++ jobs/covid_trends_job.py
@@ -1,11 +1,16 @@
-'''
+"""
 Python Spark job that imports the latest COVID-19 hospitalization data
-'''
+"""
 import sys
 import pandas as pd
 from pyspark.sql import SparkSession
 
-from covid_analysis.transforms import *
+from covid_analysis.transforms import (
+    filter_country,
+    pivot_and_clean,
+    clean_spark_cols,
+    index_to_col,
+)
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -14,12 +19,13 @@ is_prod = len(sys.argv) >= 2 and sys.argv[1] == "--prod"
 
 # read from /tmp, subset for USA, pivot and fill missing values
 df = pd.read_csv(
-    "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv")
+    "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv"
+)
 
 df = filter_country(df)
 df = pivot_and_clean(df, fillna=0)
 df = clean_spark_cols(df)
-df = index_to_col(df, colname='date')
+df = index_to_col(df, colname="date")
 
 # Convert from Pandas to a pyspark sql DataFrame.
 df = spark.createDataFrame(df)
@@ -27,11 +33,11 @@ df = spark.createDataFrame(df)
 # only write table in production mode
 if is_prod:
     # Write to Delta Lake
-    df.write.mode('overwrite').saveAsTable('covid_stats')
+    df.write.mode("overwrite").saveAsTable("covid_stats")
     print("Covid data successfully imported.")
 
 # display sample data
 if is_prod:
-    spark.sql('select * from covid_stats').show(10)
+    spark.sql("select * from covid_stats").show(10)
 else:
     df.show(10)
